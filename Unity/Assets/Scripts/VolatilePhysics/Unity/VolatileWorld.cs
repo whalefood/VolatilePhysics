005e0a3d21a6f35a50f41aae31ffa572555f3936
JonahWallerstein <jwallers@gmail.com>
2018-09-05 22:41:55 -0400

    Adding back accidentally deleted files

---
+++ Unity/Assets/Scripts/VolatilePhysics/Unity/VolatileWorld.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using Volatile;
+
+public class VolatileWorld : MonoBehaviour 
+{
+  public static VolatileWorld Instance { get { return instance; } }
+  protected static VolatileWorld instance = null;
+
+  
+
+  [SerializeField]
+  int historyLength = 0;
+
+  [SerializeField]
+  bool doUpdate = true;
+
+  public VoltWorld World { get; private set; }
+
+  void Awake()
+  {
+    VolatileWorld.instance = this;
+    this.World = new VoltWorld(this.historyLength);
+  }
+
+  void FixedUpdate()
+  {
+    if (this.doUpdate)
+      this.World.Update();
+  }
+}
